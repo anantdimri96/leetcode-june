c687424fbb04d0c548be5e4962e4b1c05fbccd18
Anant <anant.dimri@kuvera.in>
2020-06-08 13:39:08 +0530

    week_2_q1 another method

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -2,10 +2,23 @@ class Solution {
 
 public:
 
-bool isPowerOfTwo(int n) {
+bool power_of_two_1(int n) {
 // return (n!=0)&& ((long)n & ((long)n - 1)) == 0;
   return n>0 && !((long)n & ((long)n-1));
 	}
+
+
+bool power_of_two_2(int n) {
+        int count = 0; 
+        for(; n>0; n>>=1){
+            if ( n & 1 ) {
+                count++;
+                if (count>1) return false;
+            }
+        }
+        return count==1;
+}
+
 
 
 };
